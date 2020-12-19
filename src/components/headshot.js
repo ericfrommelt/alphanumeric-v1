41c755821fe88dabb8cbb800b2f5cac85f3c5213
Eric Frommelt <ericfrommelt@gmail.com>
2020-12-19 09:34:19 -0500

    intro design

---
+++ src/components/headshot.js
@@ -0,0 +1,25 @@
+import * as React from 'react'
+import { StaticQuery, graphql } from 'gatsby'
+import Img from 'gatsby-image'
+import style from './headshot.module.css'
+
+export default () => (
+  <StaticQuery
+    query={graphql`
+      query {
+        portrait: file(relativePath: { eq: "ef_copenhagen.jpg" }) {
+          childImageSharp {
+            fluid(maxWidth: 800) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+      }
+    `}
+    render={data => (
+      <figure className={style.headshot}>
+        <Img className={style.headshotImg} fluid={data.portrait.childImageSharp.fluid} />
+      </figure>
+    )}
+  />
+)
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,17 +4,21 @@ import Layout from '../components/layout'
 import style from './index.module.css'
 import Art00001 from '../components/art/art0001'
 import Work from '../components/work'
+import Headshot from '../components/headshot'
 
 const IndexPage = (props) => {
   return (
     <Layout>
       <section className={style.intro}>
-        <p className={style.intro__copy}>
-        Hello, I’m Eric Frommelt — a Front End Developer with a background in Art Direction and Design. I love working on editorial experiences, microinteractions, and prototypes.
-        </p>
-        <figure className={style.artwork}>
+        <div className={style.intro__content}>
+          <Headshot />
+          <p className={style.intro__copy}>
+          <span className={style.intro__copy__sans}>Hello, I’m Eric Frommelt</span> — a UX Engineer with a background in Art Direction and Design. I work on editorial experiences, microinteractions, prototypes and design systems. I love integrating editorial design, photography, illustration and animation through component based code.
+          </p>
+        </div>
+        {/* <figure className={style.artwork}>
           <Art00001></Art00001>
-        </figure>
+        </figure> */}
       </section>
       <Work />
     </Layout>
