f5ba59f6aea074401f14d6f4b35da6b9579d33cf
Eric Frommelt <ericfrommelt@gmail.com>
2020-12-20 22:12:45 -0500

    setup analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,12 @@
 module.exports = {
     plugins: [
+        {
+            resolve: `gatsby-plugin-google-analytics`,
+            options: {
+                trackingId: "G-62LWH56VMD",
+                head: false,
+            },
+        },
         `gatsby-plugin-react-helmet`,
         `gatsby-transformer-sharp`,
         `gatsby-plugin-sharp`,
