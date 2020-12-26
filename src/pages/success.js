bccd7c86b84e7ff26987a3b351e167281d39784e
Eric Frommelt <ericfrommelt@gmail.com>
2020-12-26 16:13:43 -0500

    style adjustments

--- src/pages/success.js
+++ src/pages/success.js
@@ -1,4 +1,5 @@
 import * as React from 'react'
+import { Link } from 'gatsby'
 import Layout from '../components/layout'
 import style from './success.module.css'
 import Art0001 from '../components/art/art0001'
@@ -14,6 +15,7 @@ const SuccessPage = () => {
           <p className={style.successCopy}>
             for sending me a message. I'll get back to you shortly.
           </p>
+          <p>Back to <Link to="/">Home</Link></p>
           <h2 className={style.elsewhere__title}>Elsewhere &#8212;</h2>
           <ul className={style.elsewhere}>
             <li className={style.elsewhere__link}><a href="https://dribbble.com/ericfrommelt" target="_blank">Dribbble</a></li>
