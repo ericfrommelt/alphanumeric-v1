4b9e245cc2bd3419b54ffbc8666b47954d67c50f
Eric Frommelt <ericfrommelt@gmail.com>
2021-01-02 18:42:19 -0500

    collage-1

---
+++ src/pages/collage/index.js
@@ -0,0 +1,13 @@
+import * as React from 'react'
+import Layout from '../../components/layout.js'
+import AnxiousTimes from '../../components/collage/anxious-times.js'
+
+const CollagePage = () => {
+  return (
+    <Layout>
+      <AnxiousTimes />
+    </Layout>
+  )
+}
+
+export default CollagePage
